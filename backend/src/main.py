f5a2f4cd79e80a87c5ccbf9aff033d0cca659d56
Leonid Oliinyk <52128392+leo-oli@users.noreply.github.com>
2025-12-12 02:05:34 +0200

    feat: Add CORS middleware

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
-
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 
 from .database import create_db_and_tables
 from .routers import (
@@ -21,6 +22,14 @@ async def lifespan(app: FastAPI):
 
 # Create FastAPI app
 app = FastAPI(title="Omaha System API", version="0.1", lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(static.router, prefix="/api/v1")
 app.include_router(clients.router, prefix="/api/v1")
